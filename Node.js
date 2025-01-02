62506958cc64ea942b51a037347a69b68fd79e1d
rabi-m <152533446+rabi-m@users.noreply.github.com>
2025-01-03 00:00:17 +0200

    Create Node.js

---
+++ Node.js
@@ -0,0 +1,23 @@
+Wrote to /home/mohamed/devops-project/package.json:
+
+{
+  "name": "devops-project",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/rabi-m/devops-project-10.git"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/rabi-m/devops-project-10/issues"
+  },
+  "homepage": "https://github.com/rabi-m/devops-project-10#readme"
+}
+
